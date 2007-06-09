0725d56e9ad6ea6c0b0b8d4d736c853953258562
Daniel Molina Wegener <dmw@coder.cl>
2007-06-09 22:25:07 +0000

    - modified tail test

--- caffeine/test/test_tail.sh
+++ caffeine/test/test_tail.sh
@@ -10,9 +10,25 @@ echo "Sending caf_tail to background..."
 ./caf_tail test_tail.txt &
 
 echo "Sending text to the file..."
-for x in `seq 1 1 100`;
-do
-        echo "text line $x" >> "test_tail.txt"
-        sleep 1
-done
+
+caf_os=`uname -s`
+
+case $caf_os in
+        *BSD*)
+                echo "BSD system"
+                for x in `jot 100 1`;
+                do
+                        echo "text line $x" >> "test_tail.txt"
+                        sleep 1
+                done
+                ;;
+        *Linux*)
+                echo "Linux System"
+                for x in `seq 1 1 100`;
+                        do
+                        echo "text line $x" >> "test_tail.txt"
+                        sleep 1
+                done
+                ;;
+esac
 
